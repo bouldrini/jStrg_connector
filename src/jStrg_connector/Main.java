68f5ff344a0c58f1feb080f1ac1641d11994b2ff
Richard Wieditz <richardwieditz@RichardWieditz.local>
2015-12-26 23:24:28 +0100

    check out the commit for details

--- src/jStrg_connector/Main.java
+++ src/jStrg_connector/Main.java
@@ -20,14 +20,25 @@ public class Main {
     public static List<Server> SERVERS = new ArrayList<Server>();
     public static void main(String[] args) throws IOException, GeneralSecurityException {
 
-        Server jstrg_main_server = new Server("Main-Server", "127.0.0.1", 3000);
+        Server jstrg_main_server = new Server("Main-Server", "127.0.0.1", 3001);
         Main.server = jstrg_main_server;
         Main.SERVERS.add(jstrg_main_server);
 
         Path path = Paths.get("filter-mobil.jpg");
         File file = new File(path.toUri());
 
-        Answer answer = jstrg_main_server.create_user("Test", "User", "Dopboxuser", "src/jStrg_connector_2/jstrg");
+        Answer answer = jstrg_main_server.create_user("Test", "User", "Dopboxuser 1", "jstrg");
+        System.out.println(answer);
+        if(answer.m_status == Answer.status.DONE) {
+            System.out.println("CREATING USER DONE");
+            System.out.println("");
+        } else if(answer.m_status == Answer.status.ERROR){
+            System.out.println(answer.m_error_message);
+        }
+
+        System.out.println("");
+
+        answer = jstrg_main_server.create_user("Test", "User", "Dropboxuser 1", "jstrg");
         System.out.println(answer);
         if(answer.m_status == Answer.status.DONE) {
             System.out.println("CREATING USER DONE");
@@ -38,7 +49,7 @@ public class Main {
 
         System.out.println("");
 
-        answer = jstrg_main_server.upload_file(file, "/ordner_1/ordner_2", "filter-mobil.jpg", "Dropboxuser", "src/jStrg_connector_2/jstrg");
+        answer = jstrg_main_server.upload_file(file, "/ordner_1/ordner_2", "filter-mobil.jpg", "Dropboxuser 1", "jstrg");
         System.out.println(answer);
         if(answer.m_status == Answer.status.DONE) {
             System.out.println("FILE UPLOAD DONE");
@@ -48,7 +59,7 @@ public class Main {
 
         System.out.println("");
 
-        answer = jstrg_main_server.delete_file("/ordner_1/ordner_2/filter-mobil.jpg", "Dropboxuser", "src/jStrg_connector_2/jstrg");
+        answer = jstrg_main_server.delete_file("/ordner_1/ordner_2/filter-mobil.jpg", "Dropboxuser 1", "jstrg");
         System.out.println(answer);
         if(answer.m_status == Answer.status.DONE) {
             System.out.println("FILE DELETE DONE");
@@ -58,7 +69,7 @@ public class Main {
 
         System.out.println("");
 
-        answer = jstrg_main_server.upload_file(file, "/ordner_1/ordner_2", "filter-mobil.jpg", "Dropboxuser", "src/jStrg_connector_2/jstrg");
+        answer = jstrg_main_server.upload_file(file, "/ordner_1/ordner_2", "filter-mobil.jpg", "Dropboxuser 1", "jstrg");
         System.out.println(answer);
         if(answer.m_status == Answer.status.DONE) {
             System.out.println("FILE UPLOAD DONE");
@@ -68,7 +79,7 @@ public class Main {
 
         System.out.println("");
 
-        answer = jstrg_main_server.delete_file_folder("/ordner_1/ordner_2", "Dropboxuser", "src/jStrg_connector_2/jstrg");
+        answer = jstrg_main_server.delete_file_folder("/ordner_1/ordner_2", "Dropboxuser 1", "jstrg");
         System.out.println(answer);
         if(answer.m_status == Answer.status.DONE){
             System.out.println("DELETE FILE FOLDER DONE");
@@ -80,9 +91,9 @@ public class Main {
 
 
         System.out.println("TESTING DOWNLOAD");
-//                //        jstrg_main_server.get_file("lieblingsbild.jpg", "Dropbuser", "jstrg");
-//                //        jstrg_main_server.get_file("/ordner_0/file_0", "Dropboxuser", "jstrg");
-//                //        jstrg_main_server.get_file("lieblings", "Dropboxuser", "jstrg");
+//                //        jstrg_main_server.get_file("lieblingsbild.jpg", "Dropbuser 1", "jstrg");
+//                //        jstrg_main_server.get_file("/ordner_0/file_0", "Dropboxuser 1", "jstrg");
+//                //        jstrg_main_server.get_file("lieblings", "Dropboxuser 1", "jstrg");
 //                    };
 //                };
 //            };
